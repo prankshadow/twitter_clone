cf12ff3cc700d913220031c06e247fc9395e6abb
prankshadow <padmesh.sharma26@gmail.com>
2023-03-16 21:00:14 +0530

    added: name of user on everypost, delete from profile, show login_usr photos on profile

--- frontend/src/components/Middle.js
+++ frontend/src/components/Middle.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Middle.css'
 import Tweetloginuser from './Tweetloginuser'
+import { API_BASE_URL } from '../config'
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
 const Middle = () => {
+
+  const user = useSelector(state => state.userReducer)
+
+  const CONFIG_OBJ = {
+    headers: {
+      "Content-Type": "application/json",
+      "authentication": localStorage.getItem("authentication")
+    }
+  }
+
+  const [allposts, setAllposts] = useState([]);
+
+  const getmyPost = async () => {
+
+    // console.log('Get all posts');
+    const response = await axios.get(`${API_BASE_URL}/myposts`, CONFIG_OBJ);
+
+    if (response.status === 200) {
+      setAllposts(response.data)
+      // console.log(response.data);
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Some error occured while getting all user posts '
+      });
+    }
+
+  }
+
+  const deletePost = async (postId) => {
+    const response = await axios.delete(`${API_BASE_URL}/deletepost/${postId}`, CONFIG_OBJ);
+    if (response.status === 200) {
+      getmyPost();
+    }
+  }
+
+
+  useEffect(() => {
+    getmyPost();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <>
       <div className='col-md-7 pe-5'>
@@ -11,7 +57,7 @@ const Middle = () => {
           <button className='btn btn-primary edit-button float-end'>Edit Profile</button>
         </div>
         <div className='pt-2'>
-          <h1 className='user-name'>Prank Shadow</h1>
+          <h1 className='user-name'>{user.user.fullName}</h1>
         </div>
 
         <hr />
@@ -19,13 +65,15 @@ const Middle = () => {
           <h5 className='m-auto'>Tweet and replies</h5>
         </div>
         <hr />
-        <Tweetloginuser />
-        <Tweetloginuser />
-        <Tweetloginuser />
-        <Tweetloginuser />
-        <Tweetloginuser />
-        <Tweetloginuser />
-        <Tweetloginuser />
+        <div>
+          {allposts.map((post) => {
+            return (
+              <div key={post._id}>
+                <Tweetloginuser postData={post} deletePost={deletePost} />
+              </div>
+            )
+          })}
+        </div>
       </div>
     </>
   )
--- frontend/src/components/Tweetloginuser.js
+++ frontend/src/components/Tweetloginuser.js
@@ -1,29 +1,34 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 // import { Link } from 'react-router-dom'
 import './Tweetloginuser.css'
 
 const Tweet = (props) => {
+
+    const user = useSelector(state => state.userReducer)
+
     return (
         <div>
             <div className='card'>
                 <div className='d-flex p-2 d-flex justify-content-between'>
                     <div className='d-inline-flex p-2 '>
                         <img src='http://bit.ly/3yA6Z9D' className='profile-pic img-fluid' alt='profile' />
-                        <h6 className='my-auto ps-2'>Prank Shadow</h6>
+                        {/* <h6 className='my-auto ps-2'>{props.postData.author}</h6> */}
+                        <h6 className='my-auto ps-2'>{user.user.fullName}</h6>
                     </div>
                     <div className='justify-content-end my-auto me-3'>
                         <div className="btn-group dropstart">
                             <button className='btn btn-secondary' data-bs-toggle="dropdown" aria-expanded="false" style={{ 'textDecoration': 'none', 'color': 'black', 'border': 'none', 'backgroundColor': 'transparent' }}><i className="fa-solid fa-ellipsis-vertical fs-4"></i></button>
                             <ul className="dropdown-menu">
                                 <li><button className="dropdown-item">Edit</button></li>
-                                <li><button className="dropdown-item">Delete</button></li>
+                                <li><button className="dropdown-item" onClick={() => props.deletePost(props.postData._id)}>Delete</button></li>
                             </ul>
                         </div>
                     </div>
                 </div>
                 <div className='card-body'>
-                    <p className='card-text'>Descriptiond-flex justify-content-betweend-flex justify-content-betweend-flex justify-content-betweend-flex justify-content-between</p>
-                    <img src='https://bit.ly/3FpWmds' className='tweet-pic img-fluid pb-3' alt='tweet' />
+                    <p className='card-text'>{props.postData.description}</p>
+                    <img src={props.postData.image} className='tweet-pic img-fluid pb-3' alt='tweet' />
 
                     <div className='ps-3'>
                         <a href='/' className='pe-3' style={{ 'textDecoration': 'none', 'color': 'black' }}><i className="fa-regular fa-heart pe-1" style={{ 'color': 'red' }}></i>0</a>
